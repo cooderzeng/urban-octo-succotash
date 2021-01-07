156ce029d2aa885db6c4e6a50642380fe9575628
cooderzeng <48429942+cooderzeng@users.noreply.github.com>
2021-01-07 21:38:17 +0800

    Update tClawer.ts

--- src/tClawer.ts
+++ src/tClawer.ts
@@ -116,7 +116,7 @@ export const downloadImg = async ({ id, title }: Post) => {
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36",
         },
         maxRedirects: 3,
-        timeout: 20000,
+        timeout: 5000,
       });
     } catch (err) {
       console.log("response err: ", err.message);
